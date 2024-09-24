da86dcb5dd9f952b40035e901ea8be30b88fa9a0
= <=>
2024-09-24 11:41:11 +0200

    feat: profile countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
- scores.sort(function(a, b) {
+/*
+    scores.sort(function(a, b) {
         return b.score - a.score;
     });
 
@@ -16,10 +17,42 @@
         let scoreRow = document.createElement("tr");
         if (i < 3) { // apply color for top 3 ranks
             scoreRow.style.color = colors[i];
-        }
-
-        scoreRow.appendChild(rank);
-        scoreRow.appendChild(name);
-        scoreRow.appendChild(score);
-        leaderboard.appendChild(scoreRow);
     }
+
+    scoreRow.appendChild(rank);
+    scoreRow.appendChild(name);
+    scoreRow.appendChild(score);
+    leaderboard.appendChild(scoreRow);
+}
+*/
+
+let fecha = new Date(2026, 0, 1)
+let msFecha = fecha.getTime();
+let parrafoHoras = document.querySelector("#horas");
+let parrafoMinutos = document.querySelector("#minutos");
+let parrafoSegundos = document.querySelector("#segundos");
+let cuentaAtras = document.querySelector("#cuenta-atras");
+
+let intervalo = setInterval(() => {
+    let hoy = new Date().getTime();
+
+    let distancia = msFecha - hoy;
+    let msPorDia = 1000 * 60 * 60 * 24;
+    let msPorHora = 1000 * 60 *60;
+    let msPorMinuto = 1000 *60;
+    let msPorSegundo = 1000;
+
+    let dias = Math.floor(distancia / msPorDia);
+    let horas = Math.floor((distancia % msPorDia) / msPorHora);
+    let minutos = Math.floor((distancia % msPorHora) / msPorMinuto);
+    let segundos = Math.floor((distancia % msPorMinuto) / msPorSegundo);
+
+    parrafoHoras.innerText =  horas < 10 ? "0" + horas: horas;
+    parrafoMinutos.innerText = minutos < 10 ? "0" + minutos: minutos;
+    parrafoSegundos.innerText = segundos < 10 ? "0" + segundos: segundos;
+
+    if (distancia < 0) {
+        clearInterval(intervalo);
+        cuentaAtras.innerHTML = "<p class='grande'¡Dia completado!</p>"
+    }
+}, 1000)
